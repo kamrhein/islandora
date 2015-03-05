40c52e097712c1bacd5495a3962de5a23d0f2bba
Kilian Amrhein <amrhein@zib.de>
2015-03-05 19:13:23 +0100

    only show dc fields that have values set

--- theme/islandora-dublin-core-display.tpl.php
+++ theme/islandora-dublin-core-display.tpl.php
@@ -20,6 +20,7 @@
     <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields islandora-object-fields">
       <?php $row_field = 0; ?>
       <?php foreach($dc_array as $key => $value): ?>
+        <?php if ($value['value'] != ""): ?>
         <dt property="<?php print $value['dcterms']; ?>" content="<?php print filter_xss(htmlspecialchars($value['value'], ENT_QUOTES, 'UTF-8')); ?>" class="<?php print $value['class']; ?><?php print $row_field == 0 ? ' first' : ''; ?>">
           <?php print filter_xss($value['label']); ?>
         </dt>
@@ -27,6 +28,7 @@
           <?php print filter_xss($value['value']); ?>
         </dd>
         <?php $row_field++; ?>
+        <?php endif; ?>
       <?php endforeach; ?>
     </dl>
   </div>
